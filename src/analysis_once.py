7ec2784aaa0884551c05dff985015fc327360e60
flaxel <falk.puschner@arcor.de>
2021-04-22 16:14:34 +0200

    :sparkles: add evaluation for classifier

--- src/analysis_once.py
+++ src/analysis_once.py
@@ -1,8 +1,10 @@
 import pandas as pd
+from matplotlib import pyplot as plt
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.metrics import classification_report
 from sklearn.model_selection import train_test_split
+from utils import plot_confusion_matrix, plot_roc_curve, get_close_predicitions
 
 
 def main():
@@ -22,13 +24,22 @@ def main():
     train_features, test_features, train_labels, test_labels = \
         train_test_split(vectorized_features, labels, test_size=0.3)
 
-    # Prediction
+    # Training
     classifier = RandomForestClassifier()
     classifier.fit(train_features, train_labels)
 
+    # Evaluation
     prediction = classifier.predict(test_features)
+    prediction_proba = classifier.predict_proba(test_features)
+
     print(classification_report(test_labels, prediction))
-    print(confusion_matrix(test_labels, prediction))
+    close_predictions = get_close_predicitions(prediction_proba)
+    print(close_predictions)
+    print(len(close_predictions))
+
+    plot_roc_curve(test_labels, prediction_proba)
+    plot_confusion_matrix(test_labels, prediction)
+    plt.show()
 
 
 if __name__ == "__main__":
--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,11 @@
 import os
 import glob as gg
+from sklearn.metrics import confusion_matrix, roc_curve, auc
+from sklearn.metrics import ConfusionMatrixDisplay, RocCurveDisplay
+import matplotlib.pyplot as plt
 import requests
+import numpy as np
+
 
 # constants
 MODEL = "en_core_web_trf"
@@ -33,3 +38,32 @@ def slang_to_text(text):
     end_index = start_index + request.text[start_index:].find(postfix_str)
     result = request.text[start_index:end_index]
     return text if result == non_found else result
+
+
+def get_close_predicitions(y_pred, max_diff=0.05):
+    close_predictions = []
+
+    for _, value in enumerate(y_pred):
+        diff = abs(value[1]-value[0])
+        if diff <= max_diff:
+            close_predictions.append([value[0], value[1]])
+
+    return close_predictions
+
+
+def plot_confusion_matrix(y_test, y_pred):
+    matrix = confusion_matrix(y_test, y_pred)
+    classes = np.unique(y_test)
+    visualization = ConfusionMatrixDisplay(confusion_matrix=matrix, display_labels=classes)
+    visualization.plot()
+
+
+def plot_roc_curve(y_test, y_pred):
+    classes = np.unique(y_test)
+    _, axes = plt.subplots()
+
+    for i, value in enumerate(classes):
+        fpr, tpr, _ = roc_curve(y_test, y_pred[:, i], pos_label=value)
+        roc_auc = auc(fpr, tpr)
+        visualization = RocCurveDisplay(fpr=fpr, tpr=tpr, roc_auc=roc_auc)
+        visualization.plot(ax=axes, name=f"ROC Curve of class {value}")
